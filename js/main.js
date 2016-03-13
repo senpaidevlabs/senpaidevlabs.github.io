d471d437d0059316bcc38ba47564bbae46a9c054
Kenny Meyer <kenny@kennymeyer.net>
2016-03-12 22:39:45 -0300

    Site updated at: 2016-03-13 01:39:45 UTC

--- js/main.js
+++ js/main.js
@@ -140,7 +140,7 @@
 		
 		
 		if (prevPage.length > 0) {
-		var baseUrl = '/senpaidevlabs.github.io/';
+		var baseUrl = '';
 		var removedBaseUrl = prevPage.replace(baseUrl, '');
 		var prevItem = removedBaseUrl.replace(/\//g, '');
 			console.log('prev page: '+prevItem);
@@ -420,7 +420,7 @@
 			
 			setCookie('appolo_prev_page', window.location.pathname, 1);
 			
-				window.location = '/senpaidevlabs.github.io/';
+				window.location = '/';
 			
 			
 		});
